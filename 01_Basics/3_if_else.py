66b6e98928d5dc03d8c583c8416e12d4d95d901a
Tanish Mhatre <mhatretanish417@gmail.com>
2026-07-03 01:35:58 +0530

    Practice if else and control flow

---
+++ 01_Basics/3_if_else.py
@@ -0,0 +1,19 @@
+ #if else statement
+
+print("welcome to thr rollercoaster rider")
+height=int(input("what is your height?"))
+if height>=120:
+    print("you can ride a rollercoaster")
+else:
+    print("you need to get taller in order to ride")
+    
+
+#modulo operator
+a=int(input("enter a number:"))
+b=2
+if a%b==0:
+    print("enter number is even")
+else:
+    print("number is odd ")
+
+#elif statement
